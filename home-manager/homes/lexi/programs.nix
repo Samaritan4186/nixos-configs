4387d494a2d73c02314353af33a2d6b4eac41f2b
Samaritan4186 <lexiwrenx16@gmail.com>
2023-05-29 18:18:16 +0100

    tiny edit

--- home-manager/homes/lexi/programs.nix
+++ home-manager/homes/lexi/programs.nix
@@ -4,8 +4,6 @@ _: {
     waybar.enable = true;
     git = {
       enable = true;
-      userName = "Samaritan4186";
-      userEmail = "lexiwrenx16@gmail.com"
     firefox.enable = true;
     neovim.enable = true;
     # ... add more programs as you see fit
